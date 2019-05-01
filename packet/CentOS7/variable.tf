94989538bbc14a3feaf5792eaff91a064e9f66b5
Lucas de los Santos <lsantos@hashicorp.com>
2019-05-01 08:55:01 -0400

    Adding documentation notes to variables. Assigning definitions to the required variables

--- packet/CentOS7/variable.tf
+++ packet/CentOS7/variable.tf
@@ -1,5 +1,14 @@
+# This is your Packet API Auth token
 variable "auth_token" {}
+
+# This is the string of numbers when you log into Packet and get to your Project Space
+# For example, if you see your devices listed at
+# https://app.packet.net/projects/352000fb2-ee46-4673-93a8-de2c2bdba33b
+# .. then 352000fb2-ee46-4673-93a8-de2c2bdba33b is your project ID.
+
 variable "project_id" {}
+
+# Path to your ssh_key. Create a new one and use it solely for Packet.
 
 variable "ssh_key" {
   type = "string"
