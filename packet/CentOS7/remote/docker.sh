66ce729f967a315389cdf262c5586dd20db1ba45
Lucas de los Santos <lsantos@hashicorp.com>
2019-05-01 12:42:27 -0400

    removing sudo as packet uses root. Todo would be disable native root user and add a user on sudoer

--- packet/CentOS7/remote/docker.sh
+++ packet/CentOS7/remote/docker.sh
@@ -2,19 +2,16 @@
 set -e
 
 # Install docker
-sudo yum check-update
+yum check-update
 curl -fsSL https://get.docker.com/ | sh
-sudo systemctl start docker
-sudo systemctl status docker
-sudo systemctl enable docker
-
-#Executing Docker without sudo
-sudo usermod -aG docker $(whoami)
+systemctl start docker
+systemctl status docker
+systemctl enable docker
 
 #Test Docker
-sudo docker run hello-world
+docker run hello-world
 
 # Install docker compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 docker-compose --version
